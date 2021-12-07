f4a34fec61e67a94249e95f3eab82c801e500adb
toddr <todd.romkema@gmail.com>
2021-12-07 15:49:04 -0600

    Fixed naming issues preventing project from working

---
+++ ExceptionalPractice/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExceptionalPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+
+
+        Double[] temps = new double[] { };
+        Double firstTemp;
+        try
+        {
+            firstTemp = temps[0];
+        }
+        catch(IndexOutOfRangeException e)
+            {
+                Console.WriteLine("Value of firstTemp not set");
+            }
+    }
+    }
+}
---
+++ csharp-web-dev-lsn9exceptions/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+//Exercises for the Exceptions Chapter
+
+namespace csharp_web_dev_lsn9exceptions
+{
+    class Program
+    {
+        
+        static double Divide(double x, double y)
+        {
+            double ratio;
+            if (y != 0)
+            {
+                ratio = x / y;
+                return ratio;
+            }
+            else
+            {
+                try
+                {
+                    ratio = x / y;
+                    return ratio;
+                }
+                catch (DivideByZeroException e)
+                {
+                    Console.WriteLine("Cannot divide by 0 " +
+                        "");
+                    throw new ArgumentOutOfRangeException("y cannot be 0.");
+                }
+                return x;
+            }
+        }
+
+        /*static int CheckFileExtension(string fileName)
+        {
+            // Write your code here!
+        }
+        */
+
+        static void Main(string[] args)
+        {
+            // Test out your Divide() function here!
+
+            // Test out your CheckFileExtension() function here!
+            Dictionary<string, string> students = new Dictionary<string, string>();
+            students.Add("Carl", "Program.cs");
+            students.Add("Brad", "");
+            students.Add("Elizabeth", "MyCode.cs");
+            students.Add("Stefanie", "CoolProgram.cs");
+
+
+        }
+    }
+}
